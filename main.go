d8516ec0138a03dd2a9e34d8a9725d43799a6062
RuckusIntern <hsuan.yeh@commscope.com>
2022-07-26 07:28:43 +0000

    milestone

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"time"
+	"encoding/json"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gofiber/fiber/v2"
@@ -35,10 +36,10 @@ type Errata struct {
 type CVE struct {
 	Package          		string `json:"package"`
 	Cve_name       			string `json:"cve_name"`
-	Cvss_score    			sql.NullString `json:"cvss_score"`
+	Severity 				string `json:"severity"`
+	Cvss_score    			float32 `json:"cvss_score"`
 	Cvss_status        		string `json:"cvss_status"`
 	Cve_url 				string `json:"cve_url"`
-	Severity 				string `json:"severity"`
 	Vulnerabilities_status 	string `json:"vulnerabilities_status"`
 	Vulnerabilities_url 	string `json:"vulnerabilities_url"`
 	Platform 				string `json:"platform"`
@@ -46,10 +47,10 @@ type CVE struct {
 	Package_name 			string `json:"package_name"`
 	Package_version 		string `json:"package_version"`
 	Package_release 		string `json:"package_release"`
-	Comment 				sql.NullString `json:"comment"`
-	Solution 				sql.NullString `json:"solution"`
-	Date 					sql.NullString `json:"date"`
-	Commentator 			sql.NullString `json:"commentator"`
+	Comment 				string `json:"comment"`
+	Solution 				string `json:"solution"`
+	Date 					string `json:"date"`
+	Commentator 			string `json:"commentator"`
 }
 
 
@@ -70,98 +71,123 @@ func main() {
 	app:=fiber.New()
 	app.Use(cors.New())
 	app.Get("/get", getCve)
-	app.Get("/update", updateCve)
+	app.Get("/get/a/:package", getAPackage)
+	app.Get("/get/e/:fkpackage", getEPackage)
+	app.Put("/update/:package", updateCve)
 	log.Fatal(app.Listen(":"+"7000"))
 	
 }
 
 
 func getCve(c *fiber.Ctx) error{
-	var cves []CVE
-
-	result, err := db.Query("SELECT * FROM CVE")
+	rows, err := db.Query("SELECT * FROM CVE")
 
 	if err != nil {
 		panic(err.Error())
 	}
 
-	defer result.Close()
+	defer rows.Close()
+	var cves []CVE
 
-	for result.Next() {
+	for rows.Next() {
 		var cve CVE
 
-		err := result.Scan(&cve.Package, &cve.Cve_name, &cve.Cvss_score, &cve.Cvss_status, &cve.Cve_url, &cve.Severity, &cve.Vulnerabilities_status, &cve.Vulnerabilities_url, &cve.Platform, &cve.Release_image_name, &cve.Package_name, &cve.Package_version, &cve.Package_release, &cve.Comment, &cve.Solution, &cve.Date, &cve.Commentator)
+		err := rows.Scan(&cve.Package, &cve.Cve_name, &cve.Severity, &cve.Cvss_score, &cve.Cvss_status, &cve.Cve_url, &cve.Vulnerabilities_status, &cve.Vulnerabilities_url, &cve.Platform, &cve.Release_image_name, &cve.Package_name, &cve.Package_version, &cve.Package_release, &cve.Comment, &cve.Solution, &cve.Date, &cve.Commentator)
 
 		if err != nil {
 			panic(err.Error())
 		}
-
 		cves = append(cves, cve)
 	}
-
+	
 	return c.JSON(cves)
 }
 
 func updateCve(c *fiber.Ctx) error{
 	localtime := time.Now()
-	result, err := db.Query("UPDATE CVE SET comment = ?, solution = 'werew', commentator = 'intern' WHERE package = 'acl-2.2.51-14.el7'",localtime)
+	statement, err := db.Prepare("UPDATE CVE SET comment = ?, solution = ?, date = ?, commentator = ? WHERE package = ?")
+	params := c.AllParams()
+	if err != nil {
+		panic(err.Error())
+	}
+	body := c.Body()
+	keyVal := make(map[string]string)
+	json.Unmarshal(body, &keyVal)
+
+	newComment := keyVal["comment"]
+	newSolution := keyVal["solution"]
+	newCommentator := keyVal["commentator"]
+
+	if newComment == "" {
+		return c.JSON(fiber.Map{"message": "Comment cannot be blank"})
+	}
+
+	if newSolution == "" {
+		return c.JSON(fiber.Map{"message": "Solution cannot be blank"})
+	}
+
+	if newCommentator == "" {
+		return c.JSON(fiber.Map{"message": "Commentator cannot be blank"}) 
+	}
+	
+
+	_, err = statement.Exec(newComment, newSolution, localtime, newCommentator, params["package"])
 
 	if err != nil {
 		panic(err.Error())
 	}
 
-	defer result.Close()
-
-	return c.JSON(fiber.Map{
-		"message" : "success",
-	})
+	return c.JSON(fiber.Map{"message": "successfully", "content": body})
 }
 
-// func updateComment(w http.ResponseWriter, r *http.Request) {
-// 	params := mux.Vars(r)
+func getAPackage(c *fiber.Ctx) error{
+	
+	params := c.AllParams()
 
-// 	statement, err := db.Prepare("UPDATE CVE SET comment = ?, solution = ?, commentator = ? WHERE package = ?")
+	rows, err := db.Query("select * from AffectedPackageStatus where package = ?", params["package"])
+	if err != nil {
+		panic(err.Error())
+	}
+	defer rows.Close()
+	var apss []AffectedPackageStatus
 
-// 	if err != nil {
-// 		panic(err.Error())
-// 	}
+	for rows.Next() {
+		var aps AffectedPackageStatus
 
-// 	body, err := ioutil.ReadAll(r.Body)
+		err := rows.Scan(&aps.Affected_package_name, &aps.Package, &aps.Package_name, &aps.Product_name, &aps.Fix_status, &aps.Impact, &aps.Cpe)
 
-// 	if err != nil {
-// 		panic(err.Error())
-// 	}
+		if err != nil {
+			panic(err.Error())
+		}
+		apss = append(apss, aps)
+	}
+	
+	return c.JSON(apss)
+}
 
-// 	keyVal := make(map[string]string)
-// 	json.Unmarshal(body, &keyVal)
+func getEPackage(c *fiber.Ctx) error{
+	
+	params := c.AllParams()
 
-// 	newComment := keyVal["comment"]
-// 	newSolution := keyVal["solution"]
-// 	newCommentator := keyVal["commentator"]
+	rows, err := db.Query("select * from Errata where fkpackage = ?", params["fkpackage"])
+	if err != nil {
+		panic(err.Error())
+	}
+	defer rows.Close()
+	var erratas []Errata
 
-// 	if newComment == "" {
-// 		w.WriteHeader(http.StatusBadRequest)
-// 		json.NewEncoder(w).Encode("Comment cannot be blank")
-// 		return
-// 	}
+	for rows.Next() {
+		var errata Errata
 
-// 	if newSolution == "" {
-// 		w.WriteHeader(http.StatusBadRequest)
-// 		json.NewEncoder(w).Encode("Solution cannot be blank")
-// 		return
-// 	}
+		err := rows.Scan(&errata.Errata_product_name, &errata.Release_date, &errata.Advisory, &errata.Fkpackage, &errata.Package, &errata.Product_name, &errata.Cpe)
 
-// 	if newCommentator == "" {
-// 		w.WriteHeader(http.StatusBadRequest)
-// 		json.NewEncoder(w).Encode("Commentator cannot be blank")
-// 		return
-// 	}
+		if err != nil {
+			panic(err.Error())
+		}
 
-// 	_, err = statement.Exec(newComment, newSolution, newCommentator, params["package"])
+		erratas = append(erratas, errata)
 
-// 	if err != nil {
-// 		panic(err.Error())
-// 	}
-
-// 	fmt.Fprintf(w, "Package = %s was updated", params["package"])
-// }
+	}
+	
+	return c.JSON(erratas)
+}
